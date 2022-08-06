366233b9f7e5d58070c1ca0d47d4a973b74908d3
mrabrahamden <66357905+mrabrahamden@users.noreply.github.com>
2022-08-06 16:13:47 +0300

    about block added

---
+++ src/components/About.tsx
@@ -0,0 +1,16 @@
+import '../styles/_about.scss';
+
+function About() {
+    return (
+        <div className="about">
+            <p>
+
+            Компания <span className='fw-bold'>СлонУм</span> – проводит конкурс для детей в котором могут
+            участвовать ребята <span className='color-main fw-bold'>всех возрастов!</span> Номинации в которых можно
+            победить, есть возможность проявить себя во всех направлениях и даже <span className='color-main fw-bold'>без художественных способностей.</span>
+            </p>
+        </div>
+    );
+}
+
+export default About;
