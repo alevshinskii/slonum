98e619f90975819035c53b0412c5ef1a2c85edf2
alevshinskii <66357905+alevshinskii@users.noreply.github.com>
2022-08-14 23:48:30 +0300

    plan block plane animations

--- src/components/Plan.tsx
+++ src/components/Plan.tsx
@@ -1,8 +1,29 @@
+import { useRef } from "react";
 import RoundBtn from "./RoundBtn";
+import useIntersection from "./useIntersection";
 
 function Plan() {
+    const plane = require("../img/single-plane.svg").default;
+
+    const planeref = useRef(null);
+    const inViewport = useIntersection(planeref, "0px"); // Trigger as soon as the element becomes visible
+    const planeElement = document.getElementById("plane");
+    const text1 = document.getElementById("plan-text1");
+    const text2 = document.getElementById("plan-text2");
+    const text3 = document.getElementById("plan-text3");
+    if (inViewport) {
+        planeElement?.classList.add("animation-plan-plane");
+
+        text1?.classList.add("animation-plan-text1");
+        text2?.classList.add("animation-plan-text2");
+        text3?.classList.add("animation-plan-text3");
+    }
+
     return (
         <div className="plan">
+            <div className="plane">
+                <img src={plane} alt="" id="plane" />
+            </div>
             <div className="container">
                 <div className="block-title">
                     <h2>
@@ -11,9 +32,10 @@ function Plan() {
                 </div>
                 <div className="plan-content">
                     <img src={require("../img/plan-line.svg").default} alt="" />
+
                     <div className="plan-chapter chapter-1">
                         <RoundBtn diameter={"80px"}>1</RoundBtn>
-                        <div>
+                        <div id="plan-text1">
                             Зарегистрируйтесь
                             <br />
                             на конкурс{" "}
@@ -24,7 +46,7 @@ function Plan() {
                     </div>
                     <div className="plan-chapter chapter-2">
                         <RoundBtn diameter={"80px"}>2</RoundBtn>
-                        <div>
+                        <div id="plan-text2" ref={planeref}>
                             Выполните все
                             <br />
                             условия конкурса
@@ -32,7 +54,7 @@ function Plan() {
                     </div>
                     <div className="plan-chapter chapter-3">
                         <RoundBtn diameter={"80px"}>3</RoundBtn>
-                        <div>
+                        <div id="plan-text3">
                             Загрузите рисунок{" "}
                             <a href="/" className="link color-main">
                                 Здесь
